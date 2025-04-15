0f98f810a2aa9ce06e968f7252dca575b832113d
HarmonicVoid <marvin.bolanos25@gmail.com>
2025-04-14 18:30:30 -0700

    Adding public images and building leftbar comp

--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,7 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const LeftBar = () => {
-  return <div>LeftBar</div>;
+  return (
+    <div className=''>
+      {/* LOGO MENU BUTTON */}
+      <div className=''>
+        {/* LOGO */}
+        <Link href='/'>
+          <Image alt='logo' src='icons/logo.svg' width={24} height={24} />
+        </Link>
+      </div>
+      {/* USER */}
+      <div className=''>user</div>
+    </div>
+  );
 };
 
 export default LeftBar;
